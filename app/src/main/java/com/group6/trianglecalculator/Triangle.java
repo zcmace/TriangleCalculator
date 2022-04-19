ad848a11cb02f35029af3e2eb973e93a9d2c2f7e
zcmace <zcmace@gmail.com>
2022-04-19 18:44:14 -0400

    Added basic triangle java class

---
+++ app/src/main/java/com/group6/trianglecalculator/Triangle.java
@@ -0,0 +1,25 @@
+package com.group6.trianglecalculator;
+
+public class Triangle {
+
+    /*
+    triangle is constructed such that each angle is directly across from its
+    side counter part (e.g. angleA is across from SideA).
+    */
+
+    //angles
+    private double angleA;
+    private double angleB;
+    private double angleC;
+
+    //sides
+    private double sideA;
+    private double sideB;
+    private double sideC;
+
+    public Triangle(){
+
+    }
+
+}
+
