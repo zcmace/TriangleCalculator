bdb9a187a19ab257e7e608d3b3cba954073f55a7
zcmace <zcmace@gmail.com>
2022-04-19 19:07:36 -0400

    added constraint layout and more triangle methods

--- app/src/main/java/com/group6/trianglecalculator/Triangle.java
+++ app/src/main/java/com/group6/trianglecalculator/Triangle.java
@@ -7,18 +7,32 @@ public class Triangle {
     side counter part (e.g. angleA is across from SideA).
     */
 
-    //angles
-    private double angleA;
-    private double angleB;
-    private double angleC;
-
-    //sides
-    private double sideA;
-    private double sideB;
-    private double sideC;
-
     public Triangle(){
 
+    }
+
+    /* Calculate the final angle of a triangle given two angles */
+    public double CalculateAAA(double angle1, double angle2)
+    {
+        double result = 180 - angle1 - angle2;
+
+        if (result <= 0)
+        {
+            System.out.println("The sum of the two angles must be less than 180 degrees");
+            return -1;
+        }
+
+        return result;
+    }
+
+    /* Calculate the sides and angles of a triangle given two angles and a side */
+    public double CalculateAAS(double angleA, double angleB, double sideA){
+
+        double angleC = 180 - angleA - angleB;
+
+        
+
+        return result;
     }
 
 }
