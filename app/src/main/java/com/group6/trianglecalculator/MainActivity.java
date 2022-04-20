c77125bc5e219dbb24bd3f5e008459c2859f5b92
zcmace <zcmace@gmail.com>
2022-04-20 03:50:10 -0400

    all theorems correctly output to debug

--- app/src/main/java/com/group6/trianglecalculator/MainActivity.java
+++ app/src/main/java/com/group6/trianglecalculator/MainActivity.java
@@ -1,11 +1,14 @@
 package com.group6.trianglecalculator;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,12 +21,25 @@ public class MainActivity extends AppCompatActivity {
     private String theorem;
     private LinearLayout inputLayout;
     private LinearLayout outputLayout;
+
+    //image view for reference triangle
+    ImageView referenceImage;
+
+    //inputs and labels
     private EditText sideAinput;
     private EditText sideBinput;
     private EditText sideCinput;
-    private EditText angleAInput;
-    private EditText anlgeBInput;
-    private EditText angleCInput;
+    private EditText angleAinput;
+    private EditText angleBinput;
+    private EditText angleCinput;
+
+    private TextView sideALabel;
+    private TextView sideBLabel;
+    private TextView sideCLabel;
+    private TextView angleALabel;
+    private TextView angleBLabel;
+    private TextView angleCLabel;
+
 
 
     @Override
@@ -59,6 +75,18 @@ public class MainActivity extends AppCompatActivity {
                 theorem = item;
 
                 switch(item){
+                    case "AAS":
+                        instantiateAAS();
+                        break;
+                    case "ASA":
+                        instantiateASA();
+                        break;
+                    case "SAS":
+                        instantiateSAS();
+                        break;
+                    case "AAA":
+                        instantiateAAA();
+                        break;
                     case "SSS":
                         instantiateSSS();
                         break;
@@ -74,24 +102,49 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //create labels and inputs
+        CreateLabelsAndInputs();
 
-
+        //bind referenceImage
+        referenceImage = findViewById(R.id.imageView);
 
     }
 
     public void calculateTriangle(View view){
         System.out.println(theorem);
-
+        EditText focus = (EditText) getCurrentFocus();
+        if (focus != null){
+            focus.onEditorAction(EditorInfo.IME_ACTION_DONE);
+        }
         //switch statement to decide theorem method
         switch(theorem){
             case "AAA":
+                triangle.CalculateAAA(
+                        Double.parseDouble(angleAinput.getText().toString()),
+                        Double.parseDouble(angleBinput.getText().toString())
+                );
                 break;
             case "AAS":
+                triangle.CalculateAAS(
+                        Double.parseDouble(angleAinput.getText().toString()),
+                        Double.parseDouble(angleBinput.getText().toString()),
+                        Double.parseDouble(sideAinput.getText().toString())
+                );
                 break;
             case "SAS":
+                triangle.CalculateSAS(
+                        Double.parseDouble(sideCinput.getText().toString()),
+                        Double.parseDouble(angleBinput.getText().toString()),
+                        Double.parseDouble(sideAinput.getText().toString())
+                );
                 break;
 
             case "ASA":
+                triangle.CalculateASA(
+                        Double.parseDouble(angleAinput.getText().toString()),
+                        Double.parseDouble(sideCinput.getText().toString()),
+                        Double.parseDouble(angleBinput.getText().toString())
+                );
                 break;
             case "SSS":
                 triangle.CalculateSSS(
@@ -107,38 +160,52 @@ public class MainActivity extends AppCompatActivity {
 
         triangle.debugLog();
     }
+    public void instantiateSAS(){
+        inputLayout.removeAllViews();
+
+        inputLayout.addView(sideCLabel);
+        inputLayout.addView(sideCinput);
+        inputLayout.addView(angleBLabel);
+        inputLayout.addView(angleBinput);
+        inputLayout.addView(sideALabel);
+        inputLayout.addView(sideAinput);
+        referenceImage.setImageResource(R.drawable.sas_white_background);
+    }
+    public void instantiateAAS(){
+        inputLayout.removeAllViews();
+
+        inputLayout.addView(angleALabel);
+        inputLayout.addView(angleAinput);
+        inputLayout.addView(angleBLabel);
+        inputLayout.addView(angleBinput);
+        inputLayout.addView(sideALabel);
+        inputLayout.addView(sideAinput);
+        referenceImage.setImageResource(R.drawable.aas_white_background);
+    }
+    public void instantiateASA(){
+        inputLayout.removeAllViews();
+
+        inputLayout.addView(angleALabel);
+        inputLayout.addView(angleAinput);
+        inputLayout.addView(sideCLabel);
+        inputLayout.addView(sideCinput);
+        inputLayout.addView(angleBLabel);
+        inputLayout.addView(angleBinput);
+        referenceImage.setImageResource(R.drawable.asa_white_background);
+    }
     public void instantiateAAA(){
+        inputLayout.removeAllViews();
+
+        inputLayout.addView(angleALabel);
+        inputLayout.addView(angleAinput);
+        inputLayout.addView(angleBLabel);
+        inputLayout.addView(angleBinput);
+        referenceImage.setImageResource(R.drawable.aaa_white_background);
 
     }
     public void instantiateSSS(){
 
         inputLayout.removeAllViews();
-
-        TextView sideALabel = new TextView(this);
-        sideALabel.setText("Enter length of Side A: ");
-        sideALabel.setTextSize(18f);
-        sideAinput = new EditText(this);
-        sideAinput.setInputType(InputType.TYPE_CLASS_NUMBER);
-        sideAinput.setTag("SideAInput");
-
-        TextView sideBLabel = new TextView(this);
-        sideBLabel.setPadding(0,20,0,0);
-        sideBLabel.setText("Enter length of Side B: ");
-        sideBLabel.setTextSize(18f);
-        sideBinput = new EditText(this);
-        sideBinput.setInputType(InputType.TYPE_CLASS_NUMBER);
-        sideBinput.setTag("SideBInput");
-
-
-        TextView sideCLabel = new TextView(this);
-        sideCLabel.setPadding(0,20,0,0);
-        sideCLabel.setText("Enter length of Side C: ");
-        sideCLabel.setTextSize(18f);
-        sideCinput = new EditText(this);
-        sideCinput.setInputType(InputType.TYPE_CLASS_NUMBER);
-        sideCinput.setTag("SideCInput");
-
-
 
         inputLayout.addView(sideALabel);
         inputLayout.addView(sideAinput);
@@ -146,13 +213,60 @@ public class MainActivity extends AppCompatActivity {
         inputLayout.addView(sideBinput);
         inputLayout.addView(sideCLabel);
         inputLayout.addView(sideCinput);
-
-
+        referenceImage.setImageResource(R.drawable.sss_white_background);
 
     }
 
+    public void CreateLabelsAndInputs(){
+        sideALabel = new TextView(this);
+        sideALabel.setText("Enter length of Side a: ");
+        sideALabel.setTextSize(18f);
+        sideAinput = new EditText(this);
+        sideAinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        sideAinput.setTag("SideAInput");
+
+        sideBLabel = new TextView(this);
+        sideBLabel.setPadding(0,20,0,0);
+        sideBLabel.setText("Enter length of Side b: ");
+        sideBLabel.setTextSize(18f);
+        sideBinput = new EditText(this);
+        sideBinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        sideBinput.setTag("SideBInput");
 
 
+        sideCLabel = new TextView(this);
+        sideCLabel.setPadding(0,20,0,0);
+        sideCLabel.setText("Enter length of Side c: ");
+        sideCLabel.setTextSize(18f);
+        sideCinput = new EditText(this);
+        sideCinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        sideCinput.setTag("SideCInput");
+
+        angleALabel = new TextView(this);
+        angleALabel.setPadding(0,20,0,0);
+        angleALabel.setText("Enter Angle A in Degrees: ");
+        angleALabel.setTextSize(18f);
+        angleAinput = new EditText(this);
+        angleAinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        angleAinput.setTag("angleAInput");
+
+        angleBLabel = new TextView(this);
+        angleBLabel.setPadding(0,20,0,0);
+        angleBLabel.setText("Enter Angle B in Degrees: ");
+        angleBLabel.setTextSize(18f);
+        angleBinput = new EditText(this);
+        angleBinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        angleBinput.setTag("angleBInput");
+
+        angleCLabel = new TextView(this);
+        angleCLabel.setPadding(0,20,0,0);
+        angleCLabel.setText("Enter Angle C in Degrees: ");
+        angleCLabel.setTextSize(18f);
+        angleCinput = new EditText(this);
+        angleCinput.setInputType(InputType.TYPE_CLASS_NUMBER);
+        angleCinput.setTag("angleCInput");
+
+    }
 
 
 }
